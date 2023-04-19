2280882175ce58191865ed6b61d3bd9ebdf1a477
Boyuan Huang <h410018@gmail.com>
2023-04-19 15:08:46 +0800

    add firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './features/counter/counterSlice'
 import type { RootState } from './app/store'
@@ -6,13 +6,88 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { AuthContext } from './context/AuthContext'
-
+import { db } from './firebase'
+import { addDoc, collection, getDocs, setDoc, updateDoc, deleteDoc, doc, query, onSnapshot, orderBy, serverTimestamp } from "firebase/firestore";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [firebaseData, setFirebaseData] = useState<{doc_id: string, data: any}[]>([])
   const value = useSelector((state: RootState) => state.counter.value)
   const { googleSignIn, googleSignOut, isSignIn } = useContext(AuthContext)
+  
+  function dealWithTime(seconds: number, nanoseconds: number) {
+    let date = null
+    let timestamp = seconds * 1000 + nanoseconds / 1000000
+    const d = new Date(timestamp)
+    date = d.toString();
+    return date
+  }
 
+  const collectionRef = collection(db, "myCollections"); // get collection 
+  
+  useEffect(() => {
+    const getData = async () => {
+      const data = await query(collectionRef) // Get multiple documents from a collection
+      onSnapshot(data, (querySnapshot) => {
+        console.log('num of docs of a collection: ', querySnapshot.docs)
+        const docDataArray = querySnapshot.docs.map((doc) => {
+          const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+          if (source === 'Local') {
+            return (
+              {
+                doc_id: doc.id,
+                data: '',
+              }
+            )
+          } else {
+            return (
+              {
+                doc_id: doc.id,
+                data: doc.data(),
+              }
+            )
+          }
+          // if(row.Time === null) {
+          //   row.Time = doc.get('Time', {serverTimestamps: 'estimate'})
+          // } 
+          }
+        )
+        console.log(docDataArray)
+        setFirebaseData(docDataArray)
+      })
+    }
+    getData()
+  }, [])
+
+  const addData = async () => {
+    try{
+      await addDoc(collectionRef, {"Time": serverTimestamp()})
+    } catch(e){
+      console.log(e)
+    } 
+  }
+  const cleanData = async () => {
+    const querySnapshot = await getDocs(collectionRef)
+    querySnapshot.forEach(async (document) => {
+      try {
+        await deleteDoc(doc(db, 'myCollections', document.id))
+      } catch (e) {
+        console.log(e)
+      }
+    });
+    alert('delete all docs of collection')
+  }
+
+  const updateData = async() => {
+    const docRef = doc(db, 'myCollections', 'myDoc');
+    try {
+      await updateDoc(docRef, {
+        Test: true
+      });
+    } catch (e) {
+      console.log(e)
+    } 
+  }
   return (
     <div className="App">
       <div>
@@ -49,6 +124,39 @@ function App() {
       <button onClick={() => googleSignOut()}>
          Google signout
       </button>
+     
+      <div>
+      <div>
+        <button onClick={() => addData()}>
+          Create data
+        </button>
+        <button onClick={() => cleanData()}>
+          Clean data
+        </button>
+        <button onClick={() => updateData()}>
+          Update data 
+        </button>
+      </div>
+        firebase data: 
+        {firebaseData.map((item, index) => {
+          return (
+            <div key={item.doc_id}>
+              {index + 1} doc_id : {item.doc_id} 
+              {
+                Object.keys(item.data).map((k) => {
+                  return (
+                  <div key={k}>key: {k} value: { 
+                    typeof(item.data[k]) !== "object" && k !== "Time" ? 
+                    item.data[k] : 
+                    dealWithTime(item.data[k]['seconds'], item.data[k]['nanoseconds'])
+                    }
+                  </div>)
+                })
+              }
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
